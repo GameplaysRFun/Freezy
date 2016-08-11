73fe00ca2d482962cc6de0a5b5f77f4e72a63dc3
Joann <joann.mondresku@gmail.com>
2016-08-11 10:30:20 +0300

    Remove unused stuff

--- class/logger.js
+++ class/logger.js
@@ -4,12 +4,6 @@ winston.loggers.add('Logger', {
     colorize: true
   }
 })
-winston.loggers.add('Debug', {
-  console: {
-    colorize: true,
-    label: 'DEBUG'
-  }
-})
 var Logger = winston.loggers.get('Logger')
 exports.log = function (msg) {
     Logger.info(msg)
