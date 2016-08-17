a131c1751004301ebcd1f8353bc06f57290198a3
Joann <joann.mondresku@gmail.com>
2016-08-17 18:58:06 +0300

    Do you even suggestions + log

--- class/logger.js
+++ class/logger.js
@@ -2,7 +2,43 @@ const winston = require('winston')
 winston.loggers.add('Logger', {
   console: {
     colorize: true
-  }
+  },
+  transports: [
+    new (winston.transports.File) (
+      {
+        filename: './logs/normal',
+        level: 'info',
+        json: false,
+        formatter: function (options) {
+          return '[' + new Date(Date.now()).toLocaleString() + '] ' + options.message
+        }
+      }
+    ),
+    new (winston.transports.File) (
+      {
+        filename: './logs/errors',
+        level: 'error',
+        json: false,
+        formatter: function (options) {
+          return '[' + new Date(Date.now()).toLocaleString() + '] ' + options.message
+        }
+      }
+    )
+  ]
+})
+winston.loggers.add('Suggest', {
+  transports: [
+    new (winston.transports.File) (
+        {
+            filename: './logs/suggestions',
+            level: 'info',
+            json: false,
+            formatter: function (options) {
+                return '[' + new Date(Date.now()).toLocaleString() + '] ' + options.message
+            }
+        }
+    )  
+  ]
 })
 var Logger = winston.loggers.get('Logger')
 exports.log = function (msg) {
@@ -16,4 +52,7 @@ exports.error = function (msg) {
 }
 exports.post = function (msg) {
     Logger.info('HTTP: ' + msg)
+}
+exports.suggest = function (msg) {
+   winston.loggers.get('Suggest').info(msg)
 }
