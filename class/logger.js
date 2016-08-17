dd715898555bf670dac702bb2adeb035d72d4742
Joann <joann.mondresku@gmail.com>
2016-08-17 19:02:21 +0300

    Oops!

--- class/logger.js
+++ class/logger.js
@@ -4,40 +4,35 @@ winston.loggers.add('Logger', {
     colorize: true
   },
   transports: [
-    new (winston.transports.File) (
-      {
-        filename: './logs/normal',
-        level: 'info',
-        json: false,
-        formatter: function (options) {
-          return '[' + new Date(Date.now()).toLocaleString() + '] ' + options.message
-        }
+    new (winston.transports.File) ({
+      name: 'info-log',
+      filename: './logs/normal',
+      level: 'info',
+      formatter: function (options) {
+        return '[' + new Date(Date.now()).toLocaleString() + '] ' + options.message
       }
-    ),
-    new (winston.transports.File) (
-      {
-        filename: './logs/errors',
-        level: 'error',
-        json: false,
-        formatter: function (options) {
-          return '[' + new Date(Date.now()).toLocaleString() + '] ' + options.message
-        }
+    }),
+    new (winston.transports.File) ({
+      name: 'error-log',
+      filename: './logs/errors',
+      level: 'error',
+      formatter: function (options) {
+        return '[' + new Date(Date.now()).toLocaleString() + '] ' + options.message
       }
-    )
+    })
   ]
 })
 winston.loggers.add('Suggest', {
   transports: [
-    new (winston.transports.File) (
-        {
-            filename: './logs/suggestions',
-            level: 'info',
-            json: false,
-            formatter: function (options) {
-                return '[' + new Date(Date.now()).toLocaleString() + '] ' + options.message
-            }
-        }
-    )  
+    new (winston.transports.File) ({
+      name: 'suggestion-log',
+      filename: './logs/suggestions',
+      level: 'info',
+      json: false,
+      formatter: function (options) {
+        return '[' + new Date(Date.now()).toLocaleString() + '] ' + options.message
+      }
+    })  
   ]
 })
 var Logger = winston.loggers.get('Logger')
