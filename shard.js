51fc6beaf4da537e932cd10b94b99ff74b4acb4f
Joann <joann.mondresku@gmail.com>
2017-02-18 09:22:05 +0200

    Create shard.js

---
+++ shard.js
@@ -0,0 +1,188 @@
+var shard = parseInt(process.env["SHARD_ID"]) + 1;
+var shards = parseInt(process.env["SHARD_COUNT"]);
+var Logger = require("./utils/Logger.js");
+Logger.log("Spawned process", `Shard ${shard}: `);
+var config = require("./config.json");
+var Discord = require("discord.js");
+const Client = new Discord.Client({
+  shardId: shard - 1,
+  shardCount: shards,
+});
+var commands = new Map();
+var path = require('path');
+var fs = require('fs');
+var db = require("./utils/DataHandler.js");
+setTimeout(() => {
+  db.initialize().catch(e => {
+    Logger.error("Database Initialization failed\n" + e, `Shard ${shard}: `);
+  });
+}, config.database.timeout);
+/**
+ * Load Custom User-made Commands
+ **/
+var files = fs.readdirSync(path.join(__dirname, "./commands/user"));
+for (var i in files) {
+  if (files[i].endsWith(".js")) {
+    var temp = require(path.join(__dirname, "./commands/user/") + files[i]);
+    for (var i in temp) {
+      commands.set(i, temp[i]);
+    }
+  }
+}
+/**
+ * Load normal commands to prevent overlapping
+ **/
+files = fs.readdirSync(path.join(__dirname, "./commands/"));
+for (var i in files) {
+  if (files[i].endsWith(".js")) {
+    var temp = require(path.join(__dirname, "./commands/") + files[i]);
+    for (var i in temp) {
+      commands.set(i, temp[i]);
+    }
+  }
+}
+function updateSites() {
+  var request = require('request');
+  request({
+    method: 'POST',
+    json: true,
+    url: 'https://bots.discord.pw/api/bots/' + Client.user.id + '/stats',
+    headers: {
+      'content-type': "application/json",
+      'Authorization': config.api.discordbots
+    },
+    body: {
+      shard_count: shards,
+      shard_id: shard - 1,
+      server_count: Client.guilds.size
+    }
+  });
+}
+function antiBotCollection (m, newGuild) {
+  var g;
+  if (m.constructor.name === "Guild") {
+    g = m;
+  } else g = m.guild;
+  let bots = g.members.filter(user => user.user.bot).size;
+  let pr = (bots / g.members.size * 100).toFixed(2);
+  if (pr > 70) {
+    g.leave();
+    Logger.log(g.name + ": Leaving a server with unhealthy human/bot ratio", `Shard ${shard}: `);
+  } else {
+    if (newGuild) Logger.log(g.name + ": Joined a server with safe human/bot ratio", `Shard ${shard}: `);
+  }
+}
+Client.login(config.login.token);
+Client.on('ready', () => {
+  Logger.log("Ready!", `Shard ${shard}: `);
+  Client.user.setGame("on Shard " + (shard) + " | " + shards);
+  Client.guilds.forEach(g => {antiBotCollection(g, false)});
+});
+Client.on("guildCreate", g => {
+  antiBotCollection(g, true);
+});
+Client.on("guildMemberAdd", m => {
+  antiBotCollection(m);
+  db.fetchGuildData(m.guild.id, {option: "welcome"}).then(r => {
+    r.toArray().then(array => {
+      if (array[0]) {
+        if (array[0].state === "true") {
+          m.guild.defaultChannel.sendMessage("Welcome **" + m.user.username + "** to **" + m.guild.name + "**! You are the " + ordinalSuffix(m.guild.memberCount) + " member!");
+        }
+      }
+    }).catch(e => {});
+  });
+  function ordinalSuffix(i) {
+    var j = i % 10,
+        k = i % 100;
+    if (j == 1 && k != 11) {
+        return i + "st";
+    }
+    if (j == 2 && k != 12) {
+        return i + "nd";
+    }
+    if (j == 3 && k != 13) {
+        return i + "rd";
+    }
+    return i + "th";
+  }
+});
+Client.on("guildMemberRemove", antiBotCollection);
+Client.on("guildDelete", g => {
+  updateSites();
+  Logger.log("Left " + g.name + ". ", `Shard ${shard}: `);
+});
+Client.on('message', m => {
+  if (!m.author.bot) {
+    if (m.content.startsWith(config.Constants.prefix)) {
+      if (m.guild) {
+        if (!m.guild.members.get(Client.user.id).permissions.hasPermission("SEND_MESSAGES")) {
+          return;
+        }
+      }
+      var level = 0;
+      var base = m.content.substr(config.Constants.prefix.length).split(" ");
+      var cmd = base[0];
+      var suffix = m.content.substr(config.Constants.prefix.length + cmd.length + 1);
+      if (m.guild) {
+        if (m.guild.ownerID === m.author.id) {
+          level = 5;
+          if (config.permissions.master.indexOf(m.author.id) > -1) {
+            level = 10;
+          }
+          go();
+        } else {
+          db.fetchGuildData(m.guild.id, {user: m.author.id}).then(r => {
+            r.toArray().then(a => {
+              if (a[0]) {
+                level = a[0].level;
+                if (config.permissions.master.indexOf(m.author.id) > -1) {
+                  level = 10;
+                }
+                go();
+              } else go();
+            }).catch(() => {go();});
+          }).catch(e => go());
+        }
+      } else {
+        if (config.permissions.master.indexOf(m.author.id) > -1) {
+          level = 10;
+        }
+        go();
+      }
+      function go () { //eslint-disable-line
+        if (level < 0) {
+          return m.author.sendMessage("**Sorry!** It seems as you are blocked in **" + m.guild.name + "**. Ask the server owner to reset your permissions.\nAdditionaly, you can still use the bot in other servers or through DMs").catch(e => {});
+        }
+        if (commands.has(cmd)) {
+          try {
+            if (commands.get(cmd).guild && !m.guild) return m.channel.sendMessage(":x: **This command is intended for in-server use!**").catch(e => {});
+            if (commands.get(cmd).level > level) {
+              m.channel.sendMessage(":x: **You do not have sufficient permissions!**\nYou need to be level " + commands.get(cmd).level + ", but you are level " + level + ".");
+            } else commands.get(cmd).fn(Client, m, suffix, db);
+            Logger.log((m.guild ? m.guild.name + ": " : "DM: ") + m.author.username + "#" + m.author.discriminator + " executed <" + cmd + (suffix ? " " + suffix : "") + ">", `Shard ${shard}: `);
+          } catch (e) {
+            Logger.error((m.guild ? m.guild.name + ": " : "DM: ") + m.author.username + "#" + m.author.discriminator + " failed to execute <" + cmd + (suffix ? " " + suffix : "") + ">", `Shard ${shard}: `);
+            Logger.log(e, `Shard ${shard}: `);
+          }
+        } else if (cmd === "help") {
+          var msgArray = [];
+          msgArray.push("**Commands List**\n");
+          commands.forEach(function(val, key) {
+            if (val.level !== 10 || !!val.secret) msgArray.push('**' + key + '** - ' + val.desc);
+          });
+          m.author.sendMessage(msgArray.join('\n')).catch(e => {
+            if (m.guild) {
+              m.channel.sendMessage("**Unable to send help!** Make sure your Direct Messages are turned on!").catch(e => {});
+            }
+          }).then(msg => {
+            if (m.guild) {
+              m.channel.sendMessage(":incoming_envelope: **Sent help to your Direct Messages!**").catch(e => {});
+            }
+          });
+          Logger.log((m.guild ? m.guild.name + ": " : "DM: ") + m.author.username + "#" + m.author.discriminator + " executed <" + cmd + (suffix ? " " + suffix : "") + ">", `Shard ${shard}: `);
+        }
+      }
+    }
+  }
+});
